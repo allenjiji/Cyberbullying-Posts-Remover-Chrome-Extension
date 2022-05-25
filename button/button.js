94ac84fe6686790da9caa3360085b7d0321180b9
allenjiji <allenjiji2604@gmail.com>
2022-05-25 12:21:33 +0530

    ACJ Button Update

---
+++ button/button.js
@@ -0,0 +1,21 @@
+
+var checkInput = document.getElementById('check');
+var on = document.getElementsByClassName('on');
+var off = document.getElementsByClassName('off');
+
+checkInput.addEventListener('click', () => {
+
+  chrome.storage.sync.get("switchStatus", ({ switchStatus }) => {
+    if (switchStatus == true) {
+      on[0].style.color = "#253b52";
+      off[0].style.color = "red";
+    }
+    else {
+      on[0].style.color = "green";
+      off[0].style.color = "#253b52";
+    }
+    switchStatus = !switchStatus
+    chrome.storage.sync.set({ switchStatus })
+
+  })
+})
--- popup.js
+++ popup.js
@@ -1,46 +1,45 @@
-let changeColorButton = document.getElementById("changeColorButton");
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColorButton.style.backgroundColor = color;
+// });
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColorButton.style.backgroundColor = color;
-});
+// changeColorButton.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({
+//     active: true,
+//     currentWindow: true,
 
-changeColorButton.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true,
+//   });
+//   chrome.scripting.executeScript({
+//     target: {
+//       tabId: tab.id
+//     },
+//     function: setPageBackgroundColor,
+//   });
+// });
 
-  });
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id
-    },
-    function: setPageBackgroundColor,
-  });
-});
-
-function setPageBackgroundColor() {
-  chrome.storage.sync.get(["color", "switchStatus"], (result) => {
-    if (result.switchStatus === false) {
-      document.body.style.backgroundColor = result.color;
-      switchStatus = true
-      chrome.storage.sync.set({ switchStatus })
-    }
-    else {
-      document.body.style.backgroundColor = '#000000';
-      switchStatus = false
-      chrome.storage.sync.set({ switchStatus })
-    }
-  });
-}
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get(["color", "switchStatus"], (result) => {
+//     if (result.switchStatus === false) {
+//       document.body.style.backgroundColor = result.color;
+//       switchStatus = true
+//       chrome.storage.sync.set({ switchStatus })
+//     }
+//     else {
+//       document.body.style.backgroundColor = '#000000';
+//       switchStatus = false
+//       chrome.storage.sync.set({ switchStatus })
+//     }
+//   });
+// }
+let toggleButton = document.getElementById("check");
 
 
 setInitialSwitchState = () => {
   chrome.storage.sync.get("switchStatus", ({ switchStatus }) => {
     if (switchStatus === true) {
-      changeColorButton.checked = true;
+      toggleButton.checked = true;
     }
     else {
-      changeColorButton.checked = false;
+      toggleButton.checked = false;
     }
   })
 
